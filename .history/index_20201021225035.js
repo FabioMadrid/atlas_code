6a6394f199bd6ba113d12784e1504b4300ca0666
Fabio Madrid <fabiomadrid@atlashome.hn>
2020-10-22 19:18:46 -0600

    Search modal completo y reponsive v1

---
+++ .history/index_20201021225035.js
@@ -0,0 +1,44 @@
+//----------- funcion para responsive design del hamburger menu --------------//
+
+    function myFunction() {
+var x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+  x.className += " responsive";
+} else {
+  x.className = "topnav";
+}
+}
+
+// ------------ funcion para el modal de busqueda inmueble-------------//
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+modal.style.display = "none";
+}
+}
+
+// ----------------- introducir icono azul en active radio bottons ---------///
+
+var casa = document.getElementById("vivienda");
+var apartamento = document.getElementById("apartamento");
+var terreno = document.getElementById("terreno");
