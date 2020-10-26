8314a59be24c7fed74595eb9b98aa0649449950a
Fabio Madrid <fabiomadrid@atlashome.hn>
2020-10-26 15:06:13 -0600

    listo modal de registro

--- index.js
+++ index.js
@@ -52,4 +52,22 @@ if (event.target == modal) {
 modal.style.display = "none";
 }
 }
+
 // ------------A DEPURAR Y UNIFICAR CODIGO PARA TENER UNA SOLA FUNCION ----------------//
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
