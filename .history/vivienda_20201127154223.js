eafb17c970ac00860fa8962453cdda686ca14adf
Fabio Madrid <fabiomadrid@atlashome.hn>
2020-11-30 10:58:53 -0600

    Vista Inmueble lista para desktop

--- .history/vivienda_20201127154223.js
+++ .history/vivienda_20201127154223.js
@@ -100,39 +100,9 @@ var prevScrollpos = window.innerHeight;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("sticky-info").style.bottom = "-60px";
+    document.getElementById("sticky-info").style.bottom = "-50px";
   } else {
     document.getElementById("sticky-info").style.bottom = "0px";
   }
   prevScrollpos = currentScrollPos;
 }
-
-//------- funcion para los carrusel de propiedades sugeridas---//
-const gap = 16;
-
-const carousel = document.getElementById("carousel"),
-  content = document.getElementById("content"),
-  next = document.getElementById("next"),
-  prev = document.getElementById("prev");
-
-next.addEventListener("click", e => {
-  carousel.scrollBy(width + gap, 0);
-  if (carousel.scrollWidth !== 0) {
-    prev.style.display = "flex";
-  }
-  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = "none";
-  }
-});
-prev.addEventListener("click", e => {
-  carousel.scrollBy(-(width + gap), 0);
-  if (carousel.scrollLeft - width - gap <= 0) {
-    prev.style.display = "none";
-  }
-  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = "flex";
-  }
-});
-
-let width = carousel.offsetWidth;
-window.addEventListener("resize", e => (width = carousel.offsetWidth));
