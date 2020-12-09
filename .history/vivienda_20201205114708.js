2709bd27e4d0f71ecbd51d24f9b8f226a199b653
Fabio Madrid <fabiomadrid@atlashome.hn>
2020-12-09 15:05:57 -0600

    Arreglado el modal de busqueda

---
+++ .history/vivienda_20201205114708.js
@@ -0,0 +1,140 @@
+// ----------------FUNCION PARA LOS MEDIA TABS DE VISTA INMUEBLE ------------------ //
+
+  function showMedia(media, tipoMedia) {
+    var i, mediacontent, medialinks;
+    mediacontent = document.getElementsByClassName("mediacontent");
+    for (i = 0; i < mediacontent.length; i++) {
+        mediacontent[i].style.display = "none";
+    }
+    medialinks = document.getElementsByClassName("medialink");
+    for (i = 0; i < medialinks.length; i++) {
+        medialinks[i].className = medialinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tipoMedia).style.display = "block";
+    media.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="open" and click on it
+document.getElementById("open").click();
+
+//------------- FUNCION PARA LAS IMAGENES DENTRO DE TABLINK - FOTO-------------//
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("imgSlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" dotactive", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " dotactive";
+}
+
+//---------------- GOOGLE MAPS ----------------//
+
+
+//-----------Funcion para los accordions de la vista inmueble ---//
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active-accordion");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+//--------------FUNCION PARA DESPLEGAR LAS CARACTERISTICAS DE LA PROPIEDAD------------//
+
+var acc1 = document.getElementsByClassName("child-accordion");
+var i1;
+
+for (i1 = 0; i1 < acc1.length; i1++) {
+  acc1[i1].addEventListener("click", function() {
+    this.classList.toggle("active-child-accordion");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+//--------------FUNCION PARA DESPLEGAR LOS FILTROS ESPECIFICOS------------//
+
+function filtrosEspecificos() {
+  var x = document.getElementById("caracteristicas");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+//-----------Funcion para el sticky-info ---//
+
+var prevScrollpos = window.innerHeight;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+console.log(currentScrollPos, " valor de posicion actual")
+console.log(prevScrollpos, " valor de posicion anterior")    
+if (100 > currentScrollPos) {
+    document.getElementById("sticky-info").style.bottom = "-60px";
+  } else {
+    document.getElementById("sticky-info").style.bottom = "0px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+//------- funcion para los carrusel de propiedades sugeridas---//
+const gap = 16;
+
+const carousel = document.getElementById("carousel"),
+  content = document.getElementById("content"),
+  next = document.getElementById("next"),
+  prev = document.getElementById("prev");
+
+next.addEventListener("click", e => {
+  carousel.scrollBy(width + gap, 0);
+  if (carousel.scrollWidth !== 0) {
+    prev.style.display = "flex";
+  }
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "none";
+  }
+});
+prev.addEventListener("click", e => {
+  carousel.scrollBy(-(width + gap), 0);
+  if (carousel.scrollLeft - width - gap <= 0) {
+    prev.style.display = "none";
+  }
+  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", e => (width = carousel.offsetWidth));
